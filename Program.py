264aa64fc4a98334ad12e60ffb8f5c04e09e00f2
nadoeda <ms.nadoeda@gmail.com>
2016-12-17 14:30:50 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,8 +1,9 @@
 from Manager import *
+from Exchange import *
 
 man = Manager()
 man.addCustomer(0)
 print("Customer send 1000")
-man.sendBanknoteCustomer(0, 1000)
+man.listCustomer[0].send(1000)
 print("Customer recv 500")
-man.getBanknoteCustomer(0, 500)
+Exchange(man.bank, man.listCustomer[0],  500)
