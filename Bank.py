7495516f82c422207edad8ab4aab3ef079d28797
nadoeda <ms.nadoeda@gmail.com>
2016-12-17 14:27:41 +0300

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -10,20 +10,15 @@ class Bank:
     def __init__(self):
         self.listAccount = []
         self.listCustomer = []
-
         self.digitalSign = DigitalSignature()
         self.risk = RISK
         self.repository = Repository()
 
     def equality(self, list):  # возвращает 1 - если номиналы купюр равны
         ratings = list[0].ratings
-        for banknote in list:
-            if (banknote.ratings != ratings):
-                return 0
-        return 1
+        all([banknote.ratings != ratings for banknote in list])
 
     def addCustomer(self, customerId):
-
         self.listCustomer.append(customerId)
         account = Account(customerId, STARTSUM)
         self.listAccount.append(account)
@@ -36,3 +31,19 @@ class Bank:
     def signRepository(self, repository):
         for banknote in repository.listBanknote:
             banknote.signature = self.digitalSign.sign(banknote)
+
+    def recv(self, banknote, id):
+        if (self.digitalSign.verify(banknote) == False):
+            print('Error - signature is not authentic')
+            return False
+        else:
+            if (self.repository.countSignature(banknote.signature) != 0):
+                print('Error - repeat use banknote')
+                return False
+            else:
+                self.repository.add(banknote)
+                self.listAccount[id].increase(banknote.ratings)
+                self.listAccount[id].write()
+                self.repository.writeCountRepository()
+                return True
+
