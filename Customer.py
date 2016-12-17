8f10c770e04c6d9461f047ee48568349f156023f
nadoeda <ms.nadoeda@gmail.com>
2016-12-17 14:28:43 +0300

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -27,8 +27,20 @@ class Customer:
     def write(self):
         print('customer: id =', self.id, ' sum = ', self.sum)
 
-    def haveBanknote(self, ratings):  # возвращает купюру с заданным номиналом, 0 - если нет таких купюр
-        if self.repository.count(ratings) != 0:
-            return self.repository.getBanknoteRatings(ratings)
+    def haveBanknote(self, ratings):
+        return self.repository.count(ratings)
+
+    def getBanknote(self, ratings):  # возвращает купюру с заданным номиналом
+        return self.repository.getBanknoteRatings(ratings)
+
+    def send(self, ratings):
+        if (self.haveBanknote(ratings)):
+            banknote = self.getBanknote(ratings)
+            if (self.bank.recv(banknote, self.id)):
+                self.decrease(banknote.ratings)
+                self.write()
+                self.repository.writeCountRepository()
+            else:
+                print('Error - Send banknote customer')
         else:
-            return 0
+            print('Error - don\'t have banknote')
