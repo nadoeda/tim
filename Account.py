6ed7a6771e5ae0bc8ad3144e324f505ceede5641
nadoeda <ms.nadoeda@gmail.com>
2016-12-24 11:02:37 +0300

    Update Account.py

--- Account.py
+++ Account.py
@@ -4,10 +4,18 @@ class Account:
         self.sum = sum
 
     def increase(self, num):
-        self.sum = self.sum + num
+        try:
+            if num > 0:
+                self.sum = self.sum + num
+        except BaseException:
+            raise ('Error - nominal is not korrect')
 
     def decrease(self, num):
-        self.sum = self.sum - num
+        try:
+            if num > 0 and self.sum > num:
+                self.sum = self.sum - num
+        except BaseException:
+            raise('Error - nominal is not korrect')
 
     def write(self):
         print('account in bank: id =', self.id, ' sum = ', self.sum)
